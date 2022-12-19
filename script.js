19f4a6652ea0d71d4a2e71b3d777a82ec7007a4e
FomasTreeman <tom@team-freeman.com>
2022-12-19 13:12:15 +0000

    user can change the colours and save button added but no saveing feature yet

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 document.addEventListener("keypress", event => {if (event.code == "Space") {newColors()};});
-document.addEventListener("click", () => newColors());
+// problem when clicking on input field changes colour.
+// document.addEventListener("click", () => newColors());
 
 var randomIndex, colorChange;
+const HEX = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 function newColors() {
     var rgb = [];
     rgb[0] = Math.floor(Math.random() * 256);
     rgb[1] = Math.floor(Math.random() * 256);
     rgb[2] = Math.floor(Math.random() * 256);
+    // current algorithm for finding satisfying colour combos.
     var color = "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")" ;
+    var rgbTwo = rgb.map(color => (255 - color));
+    console.log(rgb, rgbTwo);
+    var colorTwo = "rgb(" + rgbTwo[0] + ", " + rgbTwo[1] + ", " + rgbTwo[2] + ")" ;
+    var rgbThree = similarColor(rgbTwo);
+    rgbThree = similarColor(rgbThree);
+    colorThree = "rgb(" + rgbThree[0] + ", " + rgbThree[1] + ", " + rgbThree[2] + ")"; 
+    var rgbFour = similarColor(rgbTwo);
+    rgbFour = similarColor(rgbFour);
+    colorFour = "rgb(" + rgbFour[0] + ", " + rgbFour[1] + ", " + rgbFour[2] + ")";  
+    // loop (repetition)
     document.getElementById("one").style.setProperty("--color", color);
+    document.getElementById("hexValueOne").style.setProperty("--color", "rgb(" + invertColour(rgb) + ")");
     document.getElementById("title").style.setProperty("--color", color);
-    rgb.forEach((color, index) => {rgb[index] = 255 - color});
-    var colorTwo = "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")" ;
     document.getElementById("two").style.setProperty("--color", colorTwo);
-    var tempRgb = similarColor(rgb);
-    tempRgb = similarColor(tempRgb);
-    colorThree = "rgb(" + tempRgb[0] + ", " + tempRgb[1] + ", " + tempRgb[2] + ")"; 
+    document.getElementById("hexValueTwo").style.setProperty("--color", "rgb(" + invertColour(rgbTwo) + ")");
     document.getElementById("three").style.setProperty("--color", colorThree);
-    tempRgb = similarColor(rgb);
-    tempRgb = similarColor(tempRgb);
-    colorFour = "rgb(" + tempRgb[0] + ", " + tempRgb[1] + ", " + tempRgb[2] + ")";  
+    document.getElementById("hexValueThree").style.setProperty("--color", "rgb(" + invertColour(rgbThree) + ")");
+    document.getElementById("save").style.setProperty("--color", colorThree);
     document.getElementById("four").style.setProperty("--color", colorFour);
+    document.getElementById("hexValueFour").style.setProperty("--color", "rgb(" + invertColour(rgbFour) + ")");
 }
 
 function similarColor(rgb) {
@@ -38,4 +48,12 @@ function similarColor(rgb) {
         tempRgb[randomIndex] += colorChange;
     }
     return tempRgb;
+}
+
+function updateColour(number) {
+    document.getElementById(number.toLowerCase()).style.setProperty("--color", document.getElementById("hexValue" + number).value);
+}
+
+function invertColour(rgb) {
+    return rgb.map(colour => (255 - colour)); 
 }
