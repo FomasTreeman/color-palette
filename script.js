40555144aa1903aab7e07ebe929f6dbef2eeb9c3
FomasTreeman <tom@team-freeman.com>
2022-12-19 16:22:10 +0000

    localstorage feature that persists the palette even after refresh

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-document.addEventListener("keypress", event => {if (event.code == "Space") {newColors()};});
+document.addEventListener("keypress", event => { if (event.code == "Space") { newColors() }; });
 // problem when clicking on input field changes colour.
 // document.addEventListener("click", () => newColors());
 
 var randomIndex, colorChange;
-const HEX = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+var rgb, rgbTwo, rgbThree, rgbFour;
+
+var colors = localStorage.getItem('colors');
+if (colors) {
+    var split = colors.split(';');
+    rgb = split[0].split(',');
+    rgbTwo = split[1].split(',');
+    rgbThree = split[2].split(',');
+    rgbFour = split[3].split(',');
+    setColors(rgb.map((str) => parseInt(str)), rgbTwo.map((str) => parseInt(str)), rgbThree.map((str) => parseInt(str)), rgbFour.map((str) => parseInt(str)));
+}
 
 function newColors() {
     var rgb = [];
@@ -11,32 +21,31 @@ function newColors() {
     rgb[1] = Math.floor(Math.random() * 256);
     rgb[2] = Math.floor(Math.random() * 256);
     // current algorithm for finding satisfying colour combos.
-    var color = "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")" ;
-    var rgbTwo = rgb.map(color => (255 - color));
-    console.log(rgb, rgbTwo);
-    var colorTwo = "rgb(" + rgbTwo[0] + ", " + rgbTwo[1] + ", " + rgbTwo[2] + ")" ;
-    var rgbThree = similarColor(rgbTwo);
+    rgbTwo = rgb.map(color => (255 - color));
+    rgbThree = similarColor(rgbTwo);
     rgbThree = similarColor(rgbThree);
-    colorThree = "rgb(" + rgbThree[0] + ", " + rgbThree[1] + ", " + rgbThree[2] + ")"; 
-    var rgbFour = similarColor(rgbTwo);
+    rgbFour = similarColor(rgbTwo);
     rgbFour = similarColor(rgbFour);
-    colorFour = "rgb(" + rgbFour[0] + ", " + rgbFour[1] + ", " + rgbFour[2] + ")";  
-    // loop (repetition)
-    document.getElementById("one").style.setProperty("--color", color);
+    setColors(rgb, rgbTwo, rgbThree, rgbFour);
+    localStorage.setItem('colors', rgb + ';' + rgbTwo + ';' + rgbThree + ';' + rgbFour);
+}
+
+function setColors(rgb, rgbTwo, rgbThree, rgbFour) {
+    document.getElementById("one").style.setProperty("--color", "rgb(" + rgb + ")");
     document.getElementById("hexValueOne").style.setProperty("--color", "rgb(" + invertColour(rgb) + ")");
-    document.getElementById("title").style.setProperty("--color", color);
-    document.getElementById("two").style.setProperty("--color", colorTwo);
+    document.getElementById("title").style.setProperty("--color", "rgb(" + rgb + ")");
+    document.getElementById("two").style.setProperty("--color", "rgb(" + rgbTwo + ")");
     document.getElementById("hexValueTwo").style.setProperty("--color", "rgb(" + invertColour(rgbTwo) + ")");
-    document.getElementById("three").style.setProperty("--color", colorThree);
+    document.getElementById("three").style.setProperty("--color", "rgb(" + rgbThree + ")");
     document.getElementById("hexValueThree").style.setProperty("--color", "rgb(" + invertColour(rgbThree) + ")");
-    document.getElementById("save").style.setProperty("--color", colorThree);
-    document.getElementById("four").style.setProperty("--color", colorFour);
+    document.getElementById("save").style.setProperty("--color", "rgb(" + rgbThree + ")");
+    document.getElementById("four").style.setProperty("--color", "rgb(" + rgbFour + ")");
     document.getElementById("hexValueFour").style.setProperty("--color", "rgb(" + invertColour(rgbFour) + ")");
 }
 
 function similarColor(rgb) {
     colorChange = Math.floor(Math.random() * 91 + 31);
-    var negOrPos = Math.random() < 0.5 ? -1 : 1; 
+    var negOrPos = Math.random() < 0.5 ? -1 : 1;
     colorChange *= negOrPos;
     randomIndex = Math.floor(Math.random() * 3);
     var tempRgb = rgb.map(x => x);
@@ -55,5 +64,16 @@ function updateColour(number) {
 }
 
 function invertColour(rgb) {
-    return rgb.map(colour => (255 - colour)); 
+    return rgb.map(colour => (255 - colour));
 }
+
+// function download() {
+//     var FileSaver = require(file-saver);
+//     // const link = document.createElement("a");
+//     const file = new Blob([rgb, ',', rgbTwo, ',',  rgbThree, ',',  rgbFour], { type: 'text/plain' });
+//     // link.href = URL.createObjectURL(file);
+//     // link.download = "sample.txt";
+//     // link.click();
+//     // URL.revokeObjectURL(link.href);
+//     FileSaver.saveAs(file, "colors.txt");
+// }
