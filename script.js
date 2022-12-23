c6390abca9302a1c541bcb035dbbba46609afe34
FomasTreeman <tom@team-freeman.com>
2022-12-23 09:39:56 +0000

    rgb sort algorithm

--- script.js
+++ script.js
@@ -54,6 +54,29 @@ function newColors() {
     localStorage.setItem('colors', rgb + ';' + rgbTwo + ';' + rgbThree + ';' + rgbFour);
 }
 
+// copied algorithm
+// possibility for better color sorting, rgb sort isnt very pleasing to the eye.
+
+// function RGBToHSL(r, g, b) {
+//     r /= 255;
+//     g /= 255;
+//     b /= 255;
+//     const l = Math.max(r, g, b);
+//     const s = l - Math.min(r, g, b);
+//     const h = s
+//       ? l === r
+//         ? (g - b) / s
+//         : l === g
+//         ? 2 + (b - r) / s
+//         : 4 + (r - g) / s
+//       : 0;
+//     return [
+//       60 * h < 0 ? 60 * h + 360 : 60 * h,
+//       100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0),
+//       (100 * (2 * l - s)) / 2,
+//     ];
+//   };
+
 function sortColors(colors) {
     for(let i = 0; i < colors.length; i++){
             for(let j = 0; j < colors.length - i - 1; j++){
@@ -107,7 +130,6 @@ function similarColor(rgb) {
 function updateColour(number) {
     let hex = document.getElementById("hexValue" + number)
     let column = document.getElementById(number.toLowerCase())
-    console.log(hex.value);
     validateHex(hex.value) ? column.style.setProperty("--color", hex.value) : hex.value = getHexValue(localStorage.getItem('colors').split(';')[0].split(','));
 }
 
