6696f91439b0a210481d7dc9f3492bdf8c2099f3
FomasTreeman <tom@team-freeman.com>
2023-01-03 12:47:12 +0000

    delta-e to calculate distance between colours for ordering

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ var hexOne = document.getElementById("hexValueOne");
 var hexTwo = document.getElementById("hexValueTwo");
 var hexThree = document.getElementById("hexValueThree");
 var hexFour = document.getElementById("hexValueFour");
-var colorPickerOne = document.getElementById("colorPickerOne");
-var colorPickerTwo = document.getElementById("colorPickerTwo");
-var colorPickerThree = document.getElementById("colorPickerThree");
-var colorPickerFour = document.getElementById("colorPickerFour");
+// var colorPickerOne = document.getElementById("colorPickerOne");
+// var colorPickerTwo = document.getElementById("colorPickerTwo");
+// var colorPickerThree = document.getElementById("colorPickerThree");
+// var colorPickerFour = document.getElementById("colorPickerFour");
 var columnValues = { 'One': [], 'Two': [], 'Three': [], 'Four': [] };
 var lockedColumns = [];
 var colors = localStorage.getItem('colors');
 
 if (colors) {
     let i = 0;
-    for (column in columnValues) {
+    for (let column in columnValues) {
         columnValues[column] = colors.split(';')[i].split(',').map((value) => parseInt(value));
-        i ++;
+        i++;
     }
     setColors(columnValues);
 }
@@ -32,9 +32,65 @@ if (matchMedia("(max-width: 700px)").matches) document.getElementById("title").i
 function randomColor() {
     // definitely can be simplified
     var rgb = [];
-    for (index in [0, 1, 2]) rgb[index] = Math.floor(Math.random() * 256);
+    for (let index in [0, 1, 2]) rgb[index] = Math.floor(Math.random() * 256);
     console.log(rgb);
     return rgb;
+}
+
+// not mine. just convert :)
+function rgbToLab(rgb) {
+    var r = rgb[0] / 255,
+        g = rgb[1] / 255,
+        b = rgb[2] / 255,
+        x, y, z;
+
+    r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+    g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+    b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+
+    x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+    y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+    z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+
+    x = (x > 0.008856) ? Math.pow(x, 1 / 3) : (7.787 * x) + 16 / 116;
+    y = (y > 0.008856) ? Math.pow(y, 1 / 3) : (7.787 * y) + 16 / 116;
+    z = (z > 0.008856) ? Math.pow(z, 1 / 3) : (7.787 * z) + 16 / 116;
+
+    return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)]
+}
+
+function organiseColors() {
+    // console.log({ L: rgbToLab(columnValues['One'])[0], A: rgbToLab(columnValues['One'])[1], B: rgbToLab(columnValues['One'])[2] });
+    var relationshipArray = {
+        'a': DeltaE.getDeltaE00(
+            { L: rgbToLab(columnValues['One'])[0], A: rgbToLab(columnValues['One'])[1], B: rgbToLab(columnValues['One'])[2] },
+            { L: rgbToLab(columnValues['Two'])[0], A: rgbToLab(columnValues['Two'])[1], B: rgbToLab(columnValues['Two'])[2] }),
+        'b': DeltaE.getDeltaE00(
+            { L: rgbToLab(columnValues['One'])[0], A: rgbToLab(columnValues['One'])[1], B: rgbToLab(columnValues['One'])[2] },
+            { L: rgbToLab(columnValues['Three'])[0], A: rgbToLab(columnValues['Three'])[1], B: rgbToLab(columnValues['Three'])[2] }),
+        'c': DeltaE.getDeltaE00(
+            { L: rgbToLab(columnValues['One'])[0], A: rgbToLab(columnValues['One'])[1], B: rgbToLab(columnValues['One'])[2] },
+            { L: rgbToLab(columnValues['Four'])[0], A: rgbToLab(columnValues['Four'])[1], B: rgbToLab(columnValues['Four'])[2] }),
+        'd': DeltaE.getDeltaE00(
+            { L: rgbToLab(columnValues['Two'])[0], A: rgbToLab(columnValues['Two'])[1], B: rgbToLab(columnValues['Two'])[2] },
+            { L: rgbToLab(columnValues['Three'])[0], A: rgbToLab(columnValues['Three'])[1], B: rgbToLab(columnValues['Three'])[2] }),
+        'e': DeltaE.getDeltaE00(
+            { L: rgbToLab(columnValues['Two'])[0], A: rgbToLab(columnValues['Two'])[1], B: rgbToLab(columnValues['Two'])[2] },
+            { L: rgbToLab(columnValues['Four'])[0], A: rgbToLab(columnValues['Four'])[1], B: rgbToLab(columnValues['Four'])[2] }),
+        'f': DeltaE.getDeltaE00(
+            { L: rgbToLab(columnValues['Three'])[0], A: rgbToLab(columnValues['Three'])[1], B: rgbToLab(columnValues['Three'])[2] },
+            { L: rgbToLab(columnValues['Four'])[0], A: rgbToLab(columnValues['Four'])[1], B: rgbToLab(columnValues['Four'])[2] }),
+    };
+    // console.log(relationshipArray);
+    var orderedDistances = sortColors2(relationshipArray);
+    // console.log(orderedDistances);
+    var order = shortestPath(orderedDistances);
+    console.log(order);
+    var colorsToSet = {};
+    for (let i in [0, 1, 2, 3]) colorsToSet[order[i]] = columnValues[order[i]];
+    console.log(colorsToSet);
+    setColors(colorsToSet);
+    localStorage.setItem('colors', columnValues['One'] + ';' + columnValues['Two'] + ';' + columnValues['Three'] + ';' + columnValues['Four']);
 }
 
 function newColors() {
@@ -66,48 +122,121 @@ function newColors() {
     } else if (lockedColumns.length == 3) {
         columnValues[unlockedColumns[0]] = similarColor(newStartingColour);
     };
+
+    organiseColors();
     // order colors brigthness
-    let brightnesss = Object.values(columnValues).map((value) => {
-        if (typeof value[0] != 'string') {
-            return value.reduce((acc, currentValue) => acc + currentValue);
-        }
-    });
-    let brightnessDictionary = [
-        { 'One': brightnesss[0] },
-        { 'Two': brightnesss[1] },
-        { 'Three': brightnesss[2] },
-        { 'Four': brightnesss[3] }
-    ]
-    // let desiredFormat = {
-    //     'One': brightnesss[0],
-    //     'Two': brightnesss[1],
-    //     'Three': brightnesss[2],
-    //     'Four': brightnesss[3]
+    // let brightnesss = Object.values(columnValues).map((value) => {
+    //     if (typeof value[0] != 'string') {
+    //         return value.reduce((acc, currentValue) => acc + currentValue);
+    //     }
+    // });
+    // let brightnessDictionary = [
+    //     { 'One': brightnesss[0] },
+    //     { 'Two': brightnesss[1] },
+    //     { 'Three': brightnesss[2] },
+    //     { 'Four': brightnesss[3] }
+    // ]
+    // // let desiredFormat = {
+    // //     'One': brightnesss[0],
+    // //     'Two': brightnesss[1],
+    // //     'Three': brightnesss[2],
+    // //     'Four': brightnesss[3]
+    // // }
+    // orderedIndexes = sortColors(brightnessDictionary);
+    // let listNewColors = {};
+    // for (object of brightnessDictionary) {
+    //     let column = Object.keys(object)[0];
+    //     listNewColors[column] = columnValues[column];
     // }
-    orderedIndexes = sortColors(brightnessDictionary);
-    let listNewColors = {};
-    for (object of brightnessDictionary) {
-        let column = Object.keys(object)[0];
-        listNewColors[column] = columnValues[column];
-    }
-    for (column of ['One', 'Two', 'Three', 'Four']) {
-        if (typeof listNewColors[column] == 'object') listNewColors[column] = columnValues[column];
-    }
-    setColors(listNewColors);
-    localStorage.setItem('colors', columnValues['One'] + ';' + columnValues['Two'] + ';' + columnValues['Three'] + ';' + columnValues['Four']);
+    // for (column of ['One', 'Two', 'Three', 'Four']) {
+    //     if (typeof listNewColors[column] == 'object') listNewColors[column] = columnValues[column];
+    // }
+    // setColors(listNewColors);
 };
 
-function sortColors(colors) {
+// function sortColors(colors) {
+//     for (let i = 0; i < colors.length; i++) {
+//         if (!lockedColumns.includes(colors[i])) {
+//             for (let j = 0; j < colors.length - i - 1; j++) {
+//                 if (Object.values(colors[j + 1])[0] > Object.values(colors[j])[0] || !lockedColumns.includes(colors[j + 1])) {
+//                     [colors[j + 1], colors[j]] = [colors[j], colors[j + 1]]
+//                 }
+//             }
+//         }
+//     };
+//     return colors.map((object) => Object.keys(object)[0]);
+// }
+
+function sortColors2(colors) {
+    colors = Object.entries(colors);
+    console.log(colors);
     for (let i = 0; i < colors.length; i++) {
-        if (!lockedColumns.includes(colors[i])) {
-            for (let j = 0; j < colors.length - i - 1; j++) {
-                if (Object.values(colors[j + 1])[0] > Object.values(colors[j])[0] || !lockedColumns.includes(colors[j + 1])) {
-                    [colors[j + 1], colors[j]] = [colors[j], colors[j + 1]]
-                }
+        for (let j = 0; j < colors.length - i - 1; j++) {
+            if (colors[j + 1][1] < colors[j][1]) {
+                [colors[j + 1], colors[j]] = [colors[j], colors[j + 1]]
             }
         }
     };
-    return colors.map((object) => Object.keys(object)[0]);
+    return colors;
+}
+
+function orderColumns(column, oppositeColumn, shortestPath) {
+    var order = [];
+    var distanceKey = {
+        'a': ['One', 'Two'],
+        'b': ['One', 'Three'],
+        'c': ['One', 'Four'],
+        'd': ['Two', 'Three'],
+        'e': ['Two', 'Four'],
+        'f': ['Three', 'Four']
+    };
+    if (distanceKey[column][0] == distanceKey[shortestPath]) {
+        order.push(distanceKey[column][1]);
+        order.push(distanceKey[column][0]);
+    } else {
+        order.push(distanceKey[column][0]);
+        order.push(distanceKey[column][1]);
+    }
+        order.push(distanceKey[shortestPath][1]);
+        distanceKey[shortestPath][1] == distanceKey[oppositeColumn][0] ? order.push(distanceKey[oppositeColumn][1]) : order.push(distanceKey[oppositeColumn][0]);
+    return order;
+    }
+
+function shortestPath(sortedDistances) {
+    var sortedDistancesObj = Object.fromEntries(sortedDistances);
+    var possibles = [];
+    var shortestPath = '';
+    console.log(sortedDistances[0][0]); 
+    switch (sortedDistances[0][0]) {
+        case 'a':
+            possibles = [sortedDistancesObj['b'], sortedDistancesObj['c'], sortedDistancesObj['d'], sortedDistancesObj['e']];
+            shortestPath = sortedDistances[sortedDistances.findIndex((element) => element[1] == Math.min(...possibles))][0];
+            console.log(shortestPath)
+            return orderColumns('a', 'f', shortestPath);
+        case 'b':
+            possibles = [sortedDistancesObj['a'], sortedDistancesObj['c'], sortedDistancesObj['d'], sortedDistancesObj['f']];
+            shortestPath = sortedDistances[sortedDistances.findIndex((element) => element[1] == Math.min(...possibles))][0];
+            return orderColumns('b', 'e', shortestPath);
+        case 'c':
+            possibles = [sortedDistancesObj['a'], sortedDistancesObj['b'], sortedDistancesObj['e'], sortedDistancesObj['f']];
+            shortestPath = sortedDistances[sortedDistances.findIndex((element) => element[1] == Math.min(...possibles))][0];
+            return orderColumns('c', 'd', shortestPath);
+        case 'd':
+            possibles = [sortedDistancesObj['a'], sortedDistancesObj['b'], sortedDistancesObj['e'], sortedDistancesObj['f']];
+            console.log(sortedDistances.findIndex((element) => element[1] == Math.min(...possibles)));
+            shortestPath = sortedDistances[sortedDistances.findIndex((element) => element[1] == Math.min(...possibles))][0];
+            return orderColumns('d', 'c', shortestPath);
+        case 'e':
+            possibles = [sortedDistancesObj['a'], sortedDistancesObj['c'], sortedDistancesObj['d'], sortedDistancesObj['f']];
+            shortestPath = sortedDistances[sortedDistances.findIndex((element) => element[1] == Math.min(...possibles))][0];
+            return orderColumns('e', 'b', shortestPath);
+        case 'f':
+            possibles = [sortedDistancesObj['b'], sortedDistancesObj['c'], sortedDistancesObj['d'], sortedDistancesObj['e']];
+            shortestPath = sortedDistances[sortedDistances.findIndex((element) => element[1] == Math.min(...possibles))][0];
+            return orderColumns('f', 'a', shortestPath);
+    }
+
+    // return [order of columns]
 }
 
 function changeColumn(number, colorValue) {
